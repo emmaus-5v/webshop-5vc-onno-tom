bfff7e508da40f0fd3e86b0179c37b5f9c096fdb
TomNijh <112494@emmauscollege.nl>
2021-11-05 09:28:55 +0000

    3

--- db/create.sql
+++ db/create.sql
@@ -24,7 +24,7 @@ CREATE TABLE products (
 insert into products (name, description, code, price) values ('Dutch Film', 'nederlandse film', '816905633-0', 10.5);
 insert into products (name, description, code, price) values ('kleding', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.
 
-Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
+Cras velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
 
 Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.', '077030122-3', 11);
 insert into products (name, description, code, price) values ('Scarlet and the Black, The', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.
