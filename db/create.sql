67c56b535a605f9ea886ab833adac2c932f574f7
TomNijh <112494@emmauscollege.nl>
2021-12-13 22:22:29 +0000

    database
    Signed-off-by: TomNijh <112494@emmauscollege.nl>

--- db/create.sql
+++ db/create.sql
@@ -4,49 +4,77 @@
 
 DROP TABLE IF EXISTS products; 
 CREATE TABLE products (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id INTEGER PRIMARY KEY AUTOINCREMENT ,
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
-  price NUMERIC(10, 2)
+  price NUMERIC(10, 300),
+  broek_id INTEGER,
+  shirt_id INTEGER,
+  truien_id INTEGER,
+  sokken_id INTEGER,
+  schoenen_id INTEGER,
+  bundle_id INTEGER
 );
 
-CREATE TABLE broeken (
+DROP TABLE IF EXISTS broek;
+CREATE TABLE broek (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
+  code VARCHAR(15),
   soort stof TEXT,
-  naam TEXT,
-  prijs NUMERIC(40, 100),
   gemaakt TEXT,
-  omschrijving TEXT
+  merk TEXT,
+  maat NUMERIC(12, 30)
 );
 
-CREATE TABLE t-shirt (
+DROP TABLE IF EXISTS shirt;
+CREATE TABLE shirt (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  naam TEXT,
+  code VARCHAR(15),
   soort stof TEXT,
   merk TEXT, 
-  prijs NUMERIC(20, 60),
   gemaakt TEXT,
-  omschrijving TEXT
+  maat NUMERIC(12, 30)
 );
 
+DROP TABLE IF EXISTS bundle;
 CREATE TABLE bundle (
    id INTEGER PRIMARY KEY AUTOINCREMENT,
-   broek TEXT,
-   t-shirt TEXT,
-   hoodie TEXT,
-   prijs NUMERIC(100, 240)
-)
+   broek_id INTEGER,
+  shirt_id INTEGER,
+  truien_id INTEGER,
+  sokken_id INTEGER
+);
 
-CREATE TABLE hoodies (
+DROP TABLE IF EXISTS truien;
+CREATE TABLE truien (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-  naam TEXT,
+  code VARCHAR (15),
   soort stof TEXT,
   merk TEXT, 
-  prijs NUMERIC(40, 120),
   gemaakt TEXT,
-  omschrijving TEXT
-)
+  maat NUMERIC(12,30)
+);
+
+DROP TABLE IF EXISTS sokken;
+CREATE TABLE sokken(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  code VARCHAR (15),
+  soort stof TEXT,
+  merk TEXT, 
+  gemaakt TEXT,
+  maat NUMERIC(12,30)
+);
+
+DROP TABLE IF EXISTS schoenen;
+CREATE TABLE schoenen (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+  code VARCHAR (15),
+  soort stof TEXT,
+  merk TEXT, 
+  gemaakt TEXT,
+  maat NUMERIC(12,30)
+);
 
 
 
@@ -60,32 +88,31 @@ id INTEGER PRIMARY KEY AUTOINCREMENT,
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Dutch Film', 'nederlandse film', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('kleding', 'rode kleding
+insert into broek ( code, soort stof, gemaakt, merk, maat) values ('6756478-8','katoenmix', 'China', 'chasin', 16);
+insert into broek ( code, soort stof, gemaakt, merk, maat) values ('6756478-9','katoenmix', 'China', 'chasin', 16);
+insert into broek ( code, soort stof, gemaakt, merk, maat) values ('6756478-4','katoenmix', 'China', 'chasin', 16);
+insert into broek ( code, soort stof, gemaakt, merk, maat) values ('6756478-1','katoenmix', 'China', 'chasin', 16);
+insert into broek ( code, soort stof, gemaakt, merk, maat) values ('6756478-6','katoenmix', 'China', 'chasin', 16);
+insert into shirt (code, soort stof, gemaakt, merk, maat) values ('6781538-1','katoenmix', 'China', 'ellese', 16);
+insert into shirt (code, soort stof, gemaakt, merk, maat) values ('6781538-2','katoenmix', 'China', 'ellese', 16);
+insert into shirt (code, soort stof, gemaakt, merk, maat) values ('6781538-3','katoenmix', 'China', 'ellese', 16);
+insert into shirt (code, soort stof, gemaakt, merk, maat) values ('6781538-4','katoenmix', 'China', 'ellese', 16);
+insert into shirt (code, soort stof, gemaakt, merk, maat) values ('6781538-5','katoenmix', 'China', 'ellese', 16);
+insert into bundle  () values ('9085478-2', );
 
-ja velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
 
-Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('jaand the Black, The', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.
 
-Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-
-Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('broeken, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
-
-In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.
-
-Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('sokken, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('riemen', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
-
-Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 14);
-
-insert into broeken ( soort stof, naam, prijs, gemaakt ) values ('wol', 'wollen broek', 45, 'Taiwan');
-insert into t-shirt () values ()
-insert into bundle  () values ()
-insert into hoodies () values ()
-insert into products (code, name, description, price ) values ( '69', 'anti water', 'maakt je kleren waterafstotend', '7,00');
-insert into products (code, name, description, price ) values ( '70', 'geur', 'laat je kleren lekker ruiken', '4,00');
-insert into products (code, name, description, price ) values ( '71', 'klerenhanger', 'om je kleren op te hangen', '4,00');
-insert into products (code, name, description, price ) values ( '72', 'strijkplaat', 'plak platen om kreukels eruit te krijgen', '10,00');
+insert into truien (code, soort stof, gemaakt, merk, maat) values ('6796478-5','katoenmix', 'China', 'HUF', 16);
+insert into truien (code, soort stof, gemaakt, merk, maat) values ('6796478-6','katoenmix', 'China', 'HUF', 16);
+insert into truien (code, soort stof, gemaakt, merk, maat) values ('6796478-7','katoenmix', 'China', 'HUF', 16);
+insert into truien (code, soort stof, gemaakt, merk, maat) values ('6796478-8','katoenmix', 'China', 'HUF', 16);
+insert into truien (code, soort stof, gemaakt, merk, maat) values ('6796478-9','katoenmix', 'China', 'HUF', 16);
+insert into sokken (code, soort stof, gemaakt, merk, maat) values ('6785753-1', 'katoenmix', 'China', 'addidas', 16);
+insert into sokken (code, soort stof, gemaakt, merk, maat) values ('6785753-2', 'katoenmix', 'China', 'addidas', 16);
+insert into sokken (code, soort stof, gemaakt, merk, maat) values ('6785753-3', 'katoenmix', 'China', 'addidas', 16);
+insert into sokken (code, soort stof, gemaakt, merk, maat) values ('6785753-4', 'katoenmix', 'China', 'addidas', 16);
+insert into sokken (code, soort stof, gemaakt, merk, maat) values ('6785753-5', 'katoenmix', 'China', 'addidas', 16);
+insert into products (id, code, name, description, price ) values (1, '69', 'anti water', 'maakt je kleren waterafstotend', '7,00');
+insert into products (id, code, name, description, price ) values (2, '70', 'geur', 'laat je kleren lekker ruiken', '4,00');
+insert into products (id, code, name, description, price ) values (3,  '71', 'klerenhanger', 'om je kleren op te hangen', '4,00');
+insert into products (id, code, name, description, price ) values (4,  '72', 'strijkplaat', 'plak platen om kreukels eruit te krijgen', '10,00');
